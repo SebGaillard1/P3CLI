bf7409bed82ef6de0e0b7cd8dd7b1919e1f1e863
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2021-04-21 21:08:16 +0200

    Refactoring. Classe

---
+++ P3/GameBrain.swift
@@ -0,0 +1,242 @@
+//
+//  GameBrain.swift
+//  P3
+//
+//  Created by Sebastien Gaillard on 21/04/2021.
+//
+
+import Foundation
+
+class GameBrain {
+    
+    var charactersArray = [Character]() // Ce tableau se remplit a chaque fois qu'un joueur choisi un personnage. (Il contiendra donc 6 'Character' à la fin de la sélection)
+
+    var charactersAliveP1 = [Character]() // Ce tableau contiendra les perso du joueur 1 qui ont health > 0 (vivants)
+    var charactersAliveP2 = [Character]() // Ce tableau contiendra les perso du joueur 2 qui ont health > 0 (vivants)
+    
+    
+    //MARK: - Partie Attaque
+
+    // Cette méthode affiche à l'utilisateur les personnages en vie, parmi lesquels il choisi le perosnnage qui va attaquer
+    func userChooseAttacker() {
+        
+        var index = 0 // Le chiffre qui s'affiche dans la console permettant le choix du personnage
+        var charAliveAttacking = [Character]() // Tableau des personnages encore en vie du joueur qui attaque
+        var charAliveAttacked = [Character]() // Tableau des personnages encore en vie du joueur qui subit l'attaque
+        
+        gameCycle.updateArrayOfCharactersAlive() // Mise à jour des tableaux charactersAliveP1/P2 avant de les utiliser
+        
+        if gameCycle.playerPlaying == 1 { // En fonction du joueur à qui s'est le tour, j'attribue telle ou telle liste de personnages
+            charAliveAttacking = charactersAliveP1
+            charAliveAttacked = charactersAliveP2
+        } else {
+            charAliveAttacking = charactersAliveP2
+            charAliveAttacked = charactersAliveP1
+        }
+        
+        print("\nLe joueur \(gameCycle.playerPlaying) choisi le personnage qui va attaquer :")
+        
+        // Ici boucle for pour afficher la liste de personnages qui peuvent attaquer avec leurs infos utiles
+        for character in charAliveAttacking {
+            index += 1
+            print("\(index) - \(character.type) : \(character.name), \(character.arme.degats) dgts")
+        }
+        
+        let userInput = readLine() // On récupère la saisie de l'utilisateur
+        
+        switch userInput {
+        case "1":
+            print("\(charAliveAttacking[0].name) selectionné") // On avertit l'utilisateur du personnage selectionné
+            userChooseTarget(characterAttacking: charAliveAttacking[0], charactersAlive: charAliveAttacked)
+        case "2":
+            if charAliveAttacking.count > 1 { // Les cas 2 et 3 nécessitent le check d'une condition : y a t il 2 ou 3 personnages ?
+                print("\(charAliveAttacking[1].name) selectionné")
+                userChooseTarget(characterAttacking: charAliveAttacking[1], charactersAlive: charAliveAttacked)
+            } else { // S'il n'y a qu'un seul personnage encore en vie et que l'utilisateur saisi '2', c'est une saisie invalide
+                print("Saisie incorrecte, recommencez.")
+                userChooseAttacker()
+            }
+        case "3":
+            if charAliveAttacking.count > 2 {
+                print("\(charAliveAttacking[2].name) selectionné")
+                userChooseTarget(characterAttacking: charAliveAttacking[2], charactersAlive: charAliveAttacked)
+            } else {
+                print("Saisie incorrecte, recommencez.")
+                userChooseAttacker()
+            }
+        default: // Si l'utilisateur choisi autre chose que '1'/'2'/'3', c'est forcément une saisie invalide, il doit donc saisir à nouveau
+            print("Saisie incorrecte, recommencez.")
+            userChooseAttacker()
+        }
+        
+    }
+
+    // Cette méthode affiche dans la console les personnages encore en vie de l'équipe adverse et permet la sélection du personnage à attaquer (elle appelle aussila méthode attack)
+    func userChooseTarget(characterAttacking: Character, charactersAlive: [Character]) {
+        
+        var index = 0 // Le chiffre qui s'affiche dans la console permettant le choix du personnage
+        
+        // On affiche la liste des personnages encore en vie qui peuvent subir une attaque.
+        print("\nChoisissez le personnage qui va recevoir l'attaque : ")
+        for character in charactersAlive {
+            index += 1
+            print("\(index) - \(character.type) : \(character.name), \(character.life) pdv")
+        }
+        
+        let userInput = readLine() // On récupère l'entrée utilisateur
+        
+        switch userInput {
+        case "1":
+            attack(with: characterAttacking, in: charactersAlive, index: userInput!)
+        case "2":
+            if charactersAlive.count > 1 {
+                attack(with: characterAttacking, in: charactersAlive, index: userInput!)
+            } else {
+                print("Saisie incorrecte, recommencez.")
+                attack(with: characterAttacking, in: charactersAlive, index: userInput!)
+            }
+        case "3":
+            if charactersAlive.count > 2 {
+                attack(with: characterAttacking, in: charactersAlive, index: userInput!)
+            } else {
+                print("Saisie incorrecte, recommencez.")
+                userChooseTarget(characterAttacking: characterAttacking, charactersAlive: charactersAlive)
+            }
+        default:
+            print("Saisie incorrecte, recommencez.")
+            userChooseTarget(characterAttacking: characterAttacking, charactersAlive: charactersAlive)
+        }
+        
+    }
+    
+    func attack(with characterAttacking: Character, in charactersAlive: [Character], index userInput: String) {
+        
+        let index = Int(userInput)! - 1
+        randomChest(characterAttacking: characterAttacking)
+        charactersAlive[index].life -= characterAttacking.arme.degats
+        print("\(characterAttacking.name) attaque \(charactersAlive[index].name) avec \(characterAttacking.arme.name) et lui inflige \(characterAttacking.arme.degats) dégâts !")
+        gameCycle.checkIfCharacterDie(character: charactersAlive[index])
+        
+    }
+    
+    //MARK: - Partie soin
+
+    func selectCharacterHealing() {
+        
+        var index = 0
+        var charAliveHeal = [Character]() // Tableau des personnages du joueur pour le heal
+
+        
+        gameCycle.updateArrayOfCharactersAlive()
+        
+        if gameCycle.playerPlaying == 1 { // J'utilise ces variables pour avoir un seul switch
+            charAliveHeal = charactersAliveP1
+        } else {
+            charAliveHeal = charactersAliveP2
+        }
+        
+        print("\nLe joueur \(gameCycle.playerPlaying) choisi le personnage qui va soigner :")
+        
+        for character in charAliveHeal {
+            index += 1
+            print("\(index) - \(character.type) : \(character.name), \(character.heal) soin")
+        }
+        
+            let userInput = readLine()
+            
+            switch userInput {
+            case "1":
+                print("\(charAliveHeal[0].name) selected")
+                selectTargetAndHeal(characterHealing: charAliveHeal[0], charactersAlive: charAliveHeal)
+            case "2":
+                if charAliveHeal.count > 1 {
+                    print("\(charAliveHeal[1].name) selected")
+                    selectTargetAndHeal(characterHealing: charAliveHeal[1], charactersAlive: charAliveHeal)
+                } else {
+                    print("Saisie incorrecte, recommencez.")
+                    selectCharacterHealing()
+                }
+            case "3":
+                if charAliveHeal.count > 2 {
+                    print("\(charAliveHeal[2].name) selected")
+                    selectTargetAndHeal(characterHealing: charAliveHeal[2], charactersAlive: charAliveHeal)
+                } else {
+                    print("Saisie incorrecte, recommencez.")
+                    selectCharacterHealing()
+                }
+            default:
+                print("Saisie incorrecte, recommencez.")
+                selectCharacterHealing()
+            }
+            
+    }
+
+
+    func selectTargetAndHeal(characterHealing: Character, charactersAlive: [Character]) {
+        
+        var index = 0
+        
+        print("\nChoisissez le personnage qui va recevoir le soin : ")
+        for character in charactersAlive {
+            index += 1
+            print("\(index) - \(character.type) : \(character.name), \(character.life) pdv")
+        }
+        
+        let userInput = readLine()
+        
+        switch userInput {
+        case "1":
+            charactersAlive[0].life += characterHealing.heal
+            print("\(characterHealing.name) soigne \(charactersAlive[0].name) de \(characterHealing.heal) pdv. Il a désormais \(charactersAlive[0].life) pdv")
+            gameCycle.endOfTurn()
+        case "2":
+            if charactersAlive.count > 1 {
+                charactersAlive[1].life += characterHealing.heal
+                print("\(characterHealing.name) soigne \(charactersAlive[1].name) de \(characterHealing.heal) pdv. Il a désormais \(charactersAlive[1].life) pdv")
+                gameCycle.endOfTurn()
+            } else {
+                print("Saisie incorrecte, recommencez.")
+                selectTargetAndHeal(characterHealing: characterHealing, charactersAlive: charactersAlive)
+            }
+        case "3":
+            if charactersAlive.count > 2 {
+                charactersAlive[2].life += characterHealing.heal
+                print("\(characterHealing.name) soigne \(charactersAlive[2].name) de \(characterHealing.heal) pdv. Il a désormais \(charactersAlive[2].life) pdv")
+                gameCycle.endOfTurn()
+            } else {
+                print("Saisie incorrecte, recommencez.")
+                selectTargetAndHeal(characterHealing: characterHealing, charactersAlive: charactersAlive)
+            }
+        default:
+            print("Saisie incorrecte, recommencez.")
+            selectTargetAndHeal(characterHealing: characterHealing, charactersAlive: charactersAlive)
+        }
+        
+    }
+
+    //MARK: - Partie coffre
+
+
+    func randomChest(characterAttacking: Character) {
+        
+        let randomInt = Int.random(in: 0...100)
+        switch randomInt {
+        case 0..<5:
+            characterAttacking.arme = Weapon(name: "Épée légendaire", degats: 50)
+            print("Incroyable, un coffre vient d'apparaître devant vous. Il contient une arme très rare !")
+        case 5..<15:
+            characterAttacking.arme = Weapon(name: "Bâton magique", degats: 40)
+            print("Superbe, un coffre vient d'apparaître devant vous. Il contient une arme rare !")
+        case 15..<30:
+            characterAttacking.arme = Weapon(name: "Marteau", degats: 30)
+            print("Un coffre vient d'apparaître devant vous !")
+        case 30..<35:
+            characterAttacking.arme = Weapon(name: "Canne à pêche", degats: 10)
+            print("Un coffre vient d'apparaître devant vous mais son contenu risque de vous déplaire... !")
+        default:
+            () // On ne fait rien
+        }
+    }
+    
+    
+}
---
+++ P3/GameCycle.swift
@@ -0,0 +1,194 @@
+//
+//  GameCycle.swift
+//  P3
+//
+//  Created by Sebastien Gaillard on 21/04/2021.
+//
+
+import Foundation
+
+var gameBrain = GameBrain()
+
+class GameCycle {
+    
+    var playerPlaying = 1 // Vaut 1 ou 2. Indique à quel joueur c'est le tour.
+    
+    var numberOfTurn = 0 // Compteur pour le nombres de tours dans la partie
+    
+    
+    func startGame() {
+        
+        print("Bienvenue dans le jeu")
+        playersChooseCharacters()
+        
+    }
+    
+    func playersChooseCharacters() {
+        
+        if gameBrain.charactersArray.count < 6 { // Quand on passe à 6, les 2 joueurs ont choisi, on lance la partie
+            if gameBrain.charactersArray.count < 3 { // Dans ce cas c'est le joueur 1 qui choisi ses personnages
+                print("\nLe joueur 1 choisi son personnage \(gameBrain.charactersArray.count + 1) : \n1 - Chevalier \n2 - Archer \n3 - Sorcier \n4 - Dragon \n5 - Ninja \n6 - Squelette \n\nTapez un chiffre puis appuyez sur Entrer.")
+            } else {
+                print("\nLe joueur 2 choisi son personnage \(gameBrain.charactersArray.count - 2) : \n1 - Chevalier \n2 - Archer \n3 - Sorcier \n4 - Dragon \n5 - Ninja \n6 - Squelette \n\nTapez un chiffre puis appuyez sur Entrer.")
+            }
+            
+            let userInput = readLine() //On récupère l'input
+            
+            switch userInput {
+            case "1":
+                gameBrain.charactersArray.append(Knight(knightName: userCharacterName()))
+                playersChooseCharacters()
+            case "2":
+                gameBrain.charactersArray.append(Archer(archerName: userCharacterName()))
+                playersChooseCharacters()
+            case "3":
+                gameBrain.charactersArray.append(Wizard(wizardName: userCharacterName()))
+                playersChooseCharacters()
+            case "4":
+                gameBrain.charactersArray.append(Dragon(dragonName: userCharacterName()))
+                playersChooseCharacters()
+            case "5":
+                gameBrain.charactersArray.append(Ninja(ninjaName: userCharacterName()))
+                playersChooseCharacters()
+            case "6":
+                gameBrain.charactersArray.append(Skeleton(skeletonName: userCharacterName()))
+                playersChooseCharacters()
+            default:
+                print("\nSaisie invalide. Saisissez un chiffre entre 1 et 6.")
+                playersChooseCharacters()
+            }
+        } else { // On passe ici quand 6 personnages ont été ajouté dans le tableau charactersList
+            startFight() // On démarre le combat
+        }
+        
+    }
+    
+    func userCharacterName() -> String {
+        
+        print("Veuillez saisir un nom pour votre personnage.")
+        
+        let userInputName = readLine() ?? "Default Name"
+        //if userInputName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        //userInputName.isEmpty
+        for character in gameBrain.charactersArray { // On parcourt chaque objet du tableau charactersList
+            if userInputName == character.name { // Si le nom saisi par l'utilisateur se trouve déjà dans la propriété d'un des objets existant, c'est true
+                print("\n'\(userInputName)' déjà utilisé, le nom doit être unique.") // On averti l'utilisateur
+                _ = userCharacterName() // On rappelle cette même fonction pourqu'il resaisisse un nom
+            }
+        }
+        return userInputName // On return le nom choisi par l'utilisateur s'il est unique
+        
+    }
+    
+    
+    
+    func startFight() {
+        
+        print("----------------------------------------------------------------------")
+        print("Le combat commence !")
+        nextTurn()
+        
+    }
+    
+    func nextTurn() {
+        
+        print("\nJoueur \(playerPlaying), veux tu :\n1 - Attaquer\n2 - Soigner")
+        
+        let userInput = readLine()
+        
+        switch userInput {
+        case "1":
+            numberOfTurn += 1
+            gameBrain.userChooseAttacker()
+        case "2":
+            numberOfTurn += 1
+            gameBrain.selectCharacterHealing()
+        default:
+            print("Saisie incorrecte. Saisissez 1 ou 2")
+            nextTurn()
+        }
+    }
+    
+    func checkIfCharacterDie(character: Character) {
+        
+        if character.life <= 0 {
+            print("\nLe personnage \(character.type) nommé \(character.name) est mort !")
+            endOfTurn()
+        } else {
+            print("Il lui reste \(character.life) pdv.")
+            endOfTurn()
+        }
+        
+    }
+    
+    func endOfTurn() {
+        
+        updateArrayOfCharactersAlive()
+        
+        if gameBrain.charactersAliveP2.count == 0 {
+            print("\nTous les personnages du joueur 2 sont morts. LE JOUEUR 1 A GAGNÉ !!!")
+            gameEnded()
+        } else if gameBrain.charactersAliveP1.count == 0 {
+            print("\nTous les personnages du joueur 1 sont morts. LE JOUEUR 2 A GAGNÉ !!!")
+            gameEnded()
+        } else {
+            if playerPlaying == 1 {
+                playerPlaying = 2
+            } else {
+                playerPlaying = 1
+            }
+            
+            nextTurn()
+        }
+        
+    }
+    
+    func updateArrayOfCharactersAlive() {
+        
+        gameBrain.charactersAliveP1.removeAll()
+        gameBrain.charactersAliveP2.removeAll()
+        
+        for character in gameBrain.charactersArray[0..<3] {
+            if character.life > 0 {
+                gameBrain.charactersAliveP1.append(character)
+            }
+        }
+        
+        for character in gameBrain.charactersArray[3..<6] {
+            if character.life > 0 {
+                gameBrain.charactersAliveP2.append(character)
+            }
+        }
+        
+    }
+    
+    
+    
+    func gameEnded() {
+        print("/n/n-----------------------------------------------------------------------")
+        print("Compte rendu de la partie :")
+        print("La partie c'est terminée en \(numberOfTurn) tours.")
+        print("Voici l'état des personnages :\n\nJoueur 1 :")
+        
+        for character in gameBrain.charactersArray[0..<3] {
+            if character.life <= 0 {
+                character.life = 0
+                print("\(character.type) : \(character.name) \(character.life) pdv")
+            } else {
+                print("\(character.type) : \(character.name) est resté vivant avec \(character.life) pdv !")
+            }
+            
+        }
+        
+        print("\nJoueur 2 :")
+        for character in gameBrain.charactersArray[3..<6] {
+            if character.life <= 0 {
+                character.life = 0
+                print("\(character.type) : \(character.name) \(character.life) pdv")
+            } else {
+                print("\(character.type) : \(character.name) est resté vivant avec \(character.life) pdv !")
+            }
+        }
+    }
+    
+}
